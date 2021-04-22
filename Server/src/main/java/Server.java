624a20f155d17e1f4b9dfca98d46c5bbfbf022e1
demigodk <bfkalombo@gmail.com>
2021-04-22 17:34:48 +0200

    Some more other code for testing

--- Server/src/main/java/Server.java
+++ Server/src/main/java/Server.java
@@ -2,5 +2,6 @@ public class Server {
     public void method() {
         System.out.println("Pop! Goes the server!");
         System.out.println("No you didn't!");
+        System.out.println("Another push");
     }
 }
--- Server/src/src/test/java/MainTest.java
+++ Server/src/src/test/java/MainTest.java
@@ -2,4 +2,7 @@ public class MainTest {
     /**
      * Server test code would go here
      */
+    void someTest(){
+        System.out.println("No code");
+    }
 }
